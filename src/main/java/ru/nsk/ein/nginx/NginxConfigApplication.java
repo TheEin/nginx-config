05b865af9829c20d2b747bc94963d1b9885ea48d
Aleksandr Kuramshin <Aleksandr.Kuramshin@billing.ru>
2019-10-08 18:06:19 +0700

    initial commit

---
+++ src/main/java/ru/nsk/ein/nginx/NginxConfigApplication.java
@@ -0,0 +1,82 @@
+package ru.nsk.ein.nginx;
+
+import com.github.odiszapc.nginxparser.NgxConfig;
+import com.github.odiszapc.nginxparser.NgxEntry;
+import org.python.core.PyString;
+import org.python.util.PythonInterpreter;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import java.util.Scanner;
+
+
+public class NginxConfigApplication {
+
+    private static PythonInterpreter py;
+
+    /**
+     * Usage: path/to/nginx.conf
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            printUsage();
+            System.exit(-1);
+        }
+        String path = args[0];
+        NgxConfig conf = parseConfig(readContent(path));
+        print(conf);
+    }
+
+    private static void print(NgxEntry e) {
+        System.out.println(e.getClass() + ": " + e);
+        if (e instanceof Iterable) {
+            ((Iterable) e).forEach(o -> print((NgxEntry) o));
+        }
+    }
+
+    private static NgxConfig parseConfig(String config) throws IOException {
+        py = new PythonInterpreter();
+        py.set("conf", new PyString(config));
+        exec("import django");
+        exec("from django.conf import settings");
+        exec("settings.configure()");
+        exec("django.setup()");
+        exec("from django import template");
+        exec("t = template.Template(conf)");
+        exec("c = template.Context()");
+        exec("res = t.render(c)");
+        String res = py.eval("res").asString();
+        try (InputStream input = new ByteArrayInputStream(res.getBytes())) {
+            return NgxConfig.read(input);
+        }
+    }
+
+    private static void exec(String code) {
+//        System.out.println(code);
+        py.exec(code);
+    }
+
+    private static String readContent(String path) throws IOException {
+        try (InputStream file = new FileInputStream(path)) {
+            return readContent(file);
+        }
+    }
+
+    private static String readContent(InputStream inputStream) {
+        return Optional.ofNullable(inputStream)
+                .map(is -> new Scanner(is).useDelimiter("\\A"))
+                .map(Scanner::next)
+                .orElse(null);
+    }
+
+    private static void printUsage() throws Exception {
+        try (InputStream usage = ClassLoader.getSystemResourceAsStream("usage.txt")) {
+            System.out.println(readContent(usage));
+        }
+    }
+}
